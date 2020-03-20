f9edab11d2628540e8f4f067f2c068d89ddd2856
wuxianucw <1840214768@qq.com>
2020-03-20 22:22:12 +0800

    一些微小的工作

--- Plugin.php
+++ Plugin.php
@@ -17,7 +17,11 @@ class CommentRuleset_Plugin implements Typecho_Plugin_Interface {
      * @throws Typecho_Plugin_Exception
      */
     public static function activate() {
-        
+        if(!is_dir(dirname(__FILE__) . '/runtime')) mkdir(dirname(__FILE__) . '/runtime');
+        if(!file_exists(dirname(__FILE__) . '/runtime/ruleset.json')) touch(dirname(__FILE__) . '/runtime/ruleset.json');
+        Typecho_Plugin::factory('Widget_Feedback')->comment = array('CommentRuleset_Plugin', 'filter');
+        Helper::addPanel(1, 'CommentRuleset/control-panel.php', '评论规则集', '评论规则集管理',' administrator');
+        return _t('插件启用成功，请配置评论规则集。');
     }
     
     /**
@@ -29,7 +33,8 @@ class CommentRuleset_Plugin implements Typecho_Plugin_Interface {
      * @throws Typecho_Plugin_Exception
      */
     public static function deactivate() {
-
+        Helper::removePanel(1, 'CommentRuleset/control-panel.php');
+        return _t('插件已禁用，已保存的规则集自动失效但并未删除。');
     }
     
     /**
@@ -41,9 +46,15 @@ class CommentRuleset_Plugin implements Typecho_Plugin_Interface {
      */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
-        /** 分类名称 */
-        $name = new Typecho_Widget_Helper_Form_Element_Text('word', NULL, 'Hello World', _t('说点什么'));
-        $form->addInput($name);
+        $panelUrl = Helper::url('CommentRuleset/control-panel.php');
+        /** MDUI 加载来源 */
+        $form->addInput(new Typecho_Widget_Helper_Form_Element_Radio('email', array(0 => '本地', 1 => 'jsDelivr'), 0, 'MDUI 加载来源', _t(<<<HTML
+            本参数用于设置评论规则集管理页面使用到的 MDUI 相关资源的加载来源，目前提供本地源和 jsDelivr 两个选项。<br>
+            如果评论规则集页面加载缓慢，可以尝试更改此项目。<br>
+            如果您不知道此项目的用途，保持默认即可。（反正也不影响规则集配置）<br>
+            如果您希望配置评论规则集，请移步<a href="{$panelUrl}" target="_blank">「控制台 -> 评论规则集」</a>。
+HTML
+        )));
     }
     
     /**
@@ -56,12 +67,12 @@ class CommentRuleset_Plugin implements Typecho_Plugin_Interface {
     public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
     
     /**
-     * 插件实现方法
+     * 评论过滤
      * 
      * @access public
      * @return void
      */
-    public static function render() {
+    public static function filter() {
         
     }
 }
