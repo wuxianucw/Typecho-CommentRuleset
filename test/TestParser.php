589becdaad2444edc86135717069df74aed80dd6
wuxianucw <1840214768@qq.com>
2020-05-16 10:51:51 +0800

    Test

--- test/TestParser.php
+++ test/TestParser.php
@@ -5,12 +5,18 @@ define('__TYPECHO_ROOT_DIR__', '/');
 require '../libs/RuleCompiler.php';
 $input = <<<EOF
 # RuleCompiler Test
-[uid==1]:accept!
+[uid==1]:[uid==1]:accept;!
  [ email <- '123' ] : review
   ! [ email <- "@" ]
      : [content~/ucw/i]:review;
      !deny;;;
 EOF;
 $compiler = new RuleCompiler();
+$compiler->parse($input);
+var_dump($compiler);
+$input = <<<EOF
+# RuleCompiler Test
+[uid==1]:!accept;
+EOF;
 $compiler->parse($input);
 var_dump($compiler);
