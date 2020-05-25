70a36b9017c59bac336cfc56413d955335f6c125
wuxianucw <1840214768@qq.com>
2020-05-25 09:23:04 +0800

    添加更复杂的测试

--- test/TestParser.php
+++ test/TestParser.php
@@ -3,20 +3,8 @@ use CommentRuleset\RuleCompiler;
 
 define('__TYPECHO_ROOT_DIR__', '/');
 require '../libs/RuleCompiler.php';
-$input = <<<EOF
-# RuleCompiler Test
-[uid!=1]:[uid==1]:accept;!
- [ email <- '123' ] : review
-  ! [ email <- "@" ]
-     : [content~/ucw/i]:review;
-     !deny;;;
-EOF;
+require 'RuleGenerator.php';
+$input = RuleGenerator::newRule();
 $compiler = new RuleCompiler();
-$compiler->parse($input);
-var_dump($compiler);
-$input = <<<EOF
-# RuleCompiler Test
-[uid==1]:!accept;
-EOF;
 $compiler->parse($input);
 var_dump($compiler);
