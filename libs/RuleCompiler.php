737cd2437925d6d4a92fdd5b869f1fefb6fa62ee
wuxianucw <1840214768@qq.com>
2020-05-27 10:28:34 +0800

    删除冗余 flag 参数

--- libs/RuleCompiler.php
+++ libs/RuleCompiler.php
@@ -744,7 +744,6 @@ class JsonTranslator extends Translator {
             if ($node->target->type == 'number') $target = $node->target->value; // 不进行类型转换以保证其在生成的 JSON 中不被转为文本
             else $target = $node->target->toString();
             $judge = array(
-                'flag' => $flag,
                 'name' => $node->name,
                 'optr' => strval($node->optr),
                 'target' => $target,
