305d725443726f19162da0ac550bce5e594d30be
wuxianucw <x-wuxian@qq.com>
2020-07-11 22:05:58 +0800

    开工 EditPage

--- Action.php
+++ Action.php
@@ -26,8 +26,35 @@ class CommentRuleset_Action extends Typecho_Widget implements Widget_Interface_D
     public function action() {
         Typecho_Widget::widget('Widget_User')->pass('administrator');
         Helper::options()->to($this->options);
+        $this->on($this->request->is('a=ruleDetails'))->ruleDetails();
         $this->on($this->request->is('a=translate'))->translate();
         $this->response->redirect($this->options->adminUrl);
+    }
+
+    /**
+     * 规则详情接口
+     * 
+     * @access public
+     * @return void
+     */
+    public function ruleDetails() {
+        if ($this->request->isGet()) {
+            $this->response->setContentType('application/json');
+            $ruid = $this->request->get('ruid', '');
+            if (strlen($ruid) != 6) {
+                $this->response->setStatus(404);
+                exit;
+            }
+            $ruleset = CommentRuleset_Plugin::getRuleset();
+            foreach ($ruleset as $rule) {
+                if ($ruid === $rule['ruid']) {
+                    echo json_encode($rule);
+                    exit;
+                }
+            }
+            $this->response->setStatus(404);
+            exit;
+        }
     }
 
     /**
--- frontend/src/components/EditPage.js
+++ frontend/src/components/EditPage.js
@@ -1,9 +1,131 @@
 import React from 'react';
+import axios from 'axios';
+import Fade from '@material-ui/core/Fade';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import useTheme from '@material-ui/core/styles/useTheme';
+
+const useStyles = makeStyles((theme) => ({
+    placeholder: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingTop: theme.spacing(8),
+    },
+}));
 
 export default function EditPage(props) {
-    const { match } = props;
+    const theme = useTheme();
+    const classes = useStyles();
+    const { match, history } = props;
+    const ruid = match.params.ruid ?? false;
+    const [initialState, setInitialState] = React.useState(ruid ? false : true);
+    const [name, setName] = React.useState("");
+    const [remark, setRemark] = React.useState("");
+    const [status, setStatus] = React.useState(["on"]);
+    const [priority, setPriority] = React.useState(10);
+    const [nameError, setNameError] = React.useState(false);
+    const [priorityError, setPriorityError] = React.useState(false);
 
-    return (
-        <div>EditPage{match.params.ruid && `(RUID=${match.params.ruid})`}</div>
+    if (!initialState) {
+        axios.get(window.__pageData.apiBase, {
+            params: {
+                a: "ruleDetails",
+                ruid: ruid,
+            },
+        }).then(({ data }) => {
+            setInitialState(true);
+            setName(data.name);
+            setRemark(data.remark);
+            setStatus(data.status);
+            setPriority(data.priority);
+        }).catch(() => {
+            history.push("/overview");
+        });
+    }
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+        setNameError(event.target.value === "");
+    };
+
+    const handleIsEnabledChange = (event) => {
+        let newStatus;
+        if (event.target.checked) {
+            newStatus = ["on"].concat(status);
+        } else {
+            newStatus = status.filter((val) => (val !== "on"));
+        }
+        setStatus(newStatus);
+    };
+
+    const handlePriorityChange = (event) => {
+        setPriority(event.target.value);
+        setPriorityError(!/^(0|[1-9][0-9]{0,4})$/.test(event.target.value));
+    };
+
+    const handleRemarkChange = (event) => {
+        setRemark(event.target.value);
+    };
+
+    return initialState ? (
+        <Container>
+            <FormGroup row>
+                <TextField
+                    value={name}
+                    onChange={handleNameChange}
+                    required
+                    error={nameError}
+                    helperText={nameError ? "规则名称不能为空" : ""}
+                    label="规则名称"
+                    variant="outlined"
+                    margin="normal"
+                    style={{ width: "50%" }}
+                />
+                <TextField
+                    value={priority}
+                    onChange={handlePriorityChange}
+                    required
+                    error={priorityError}
+                    helperText={priorityError ? "请输入一个 0 到 99999 范围内的整数" : "数值越大的规则优先级越高"}
+                    type="number"
+                    label="优先级"
+                    variant="outlined"
+                    margin="normal"
+                    style={{ width: "25%", marginLeft: theme.spacing(2) }}
+                />
+                <div style={{ flexGrow: 1 }} />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={status.indexOf("on") !== -1}
+                            onChange={handleIsEnabledChange}
+                        />
+                    }
+                    label="启用规则"
+                />
+            </FormGroup>
+            <TextField
+                value={remark}
+                onChange={handleRemarkChange}
+                multiline
+                fullWidth
+                label="规则备注"
+                helperText={remark === "怎么多行" ? "按回车" : "可以为空，允许多行"}
+                margin="normal"
+                InputLabelProps={{ shrink: true }}
+            />
+        </Container>
+    ) : (
+        <div className={classes.placeholder}>
+            <Fade in unmountOnExit style={{ transitionDelay: '800ms' }}>
+                <CircularProgress color="secondary" />
+            </Fade>
+        </div>
     );
 }
