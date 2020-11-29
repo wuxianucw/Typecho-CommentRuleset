065a03734a5eed2e933da5b83b9cc60aa47a3d41
wuxianucw <x-wuxian@qq.com>
2020-11-29 12:56:05 +0800

    :pencil2: 继续编写文档

--- frontend/src/components/GuidePage.js
+++ frontend/src/components/GuidePage.js
@@ -5,9 +5,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 import Link from '@material-ui/core/Link';
 import Divider from '@material-ui/core/Divider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
-    // TODO
+    table: {
+        minWidth: 750,
+    }
 }));
 
 export default function GuidePage() {
@@ -42,8 +51,108 @@ export default function GuidePage() {
                 <p><strong>再次强调：绝对不要手动修改或删除 <code>runtime</code> 目录下的任何文件！这样做的后果很可能是灾难性的！</strong></p>
                 <Divider />
                 <p>了解一些基本概念后，我们来尝试新增一条规则。</p>
-                <p>在“新增规则”选项卡，我们可以看到“规则名称”、“优先级”、“启用规则”、“规则备注”和“规则内容”五个配置项，下表是对它们的详细说明：</p>
+                <p>在“新增规则”选项卡，我们可以看到“规则名称”、“优先级”、“启用规则”、“规则备注”、“规则内容”以及“编译规则”六个配置项，下表是对它们的详细说明（其中“必要”的“二值”代表这个配置项一定存在且只有两种状态，不存在为空的情况）：</p>
             </Box>
+            <TableContainer component={Paper}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>配置项</TableCell>
+                            <TableCell>类型</TableCell>
+                            <TableCell>映射字段名及类型</TableCell>
+                            <TableCell>必要</TableCell>
+                            <TableCell>说明</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>规则名称</TableCell>
+                            <TableCell><code>string</code></TableCell>
+                            <TableCell><code>name: string</code></TableCell>
+                            <TableCell>是</TableCell>
+                            <TableCell>当前规则的名称。规范清晰的命名将有助于日后对规则的维护。</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>优先级</TableCell>
+                            <TableCell><code>int</code></TableCell>
+                            <TableCell><code>priority: int</code></TableCell>
+                            <TableCell>是</TableCell>
+                            <TableCell>一个大于或等于 0 且小于或等于 99999 的整数。当规则有冲突时，优先级高的规则将会优先生效。</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>启用规则</TableCell>
+                            <TableCell><code>bool</code></TableCell>
+                            <TableCell><code>status: string[]</code></TableCell>
+                            <TableCell>二值</TableCell>
+                            <TableCell>用于控制规则的启用状态。如果设置为启用，则必须编译规则。</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>规则备注</TableCell>
+                            <TableCell><code>string</code></TableCell>
+                            <TableCell><code>remark: string</code></TableCell>
+                            <TableCell>否</TableCell>
+                            <TableCell>为当前规则添加的备注信息，可以是任意内容。</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>规则内容</TableCell>
+                            <TableCell><code>RuleData | string</code></TableCell>
+                            <TableCell><code>editMode: int</code><br /><code>ruleText: string</code></TableCell>
+                            <TableCell>是</TableCell>
+                            <TableCell>当前规则的内容，决定该规则的行为。<br /><code>type RuleData = [string, string, string, string, string[], string[], string | undefined][];</code></TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>编译规则</TableCell>
+                            <TableCell><code>bool</code></TableCell>
+                            <TableCell><code>status: string[]</code></TableCell>
+                            <TableCell>二值</TableCell>
+                            <TableCell>指定是否编译这条规则。如果需要启用规则，则规则必须编译；当编译失败时，规则无法被启用。</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Box fontSize="body1.fontSize" m={1}>
+                <p>这里我们尝试新增一条在检测到评论中包含网址时，将其标为待审核的规则。这是一种很简陋的防御广告的基本方法，但是很具有代表性，可以帮助您了解规则创建的一般流程。</p>
+                <p>规则名称可以按照您的喜好填写，例如“检测网址”；规则备注同理，例如“对于包含网址的评论，将其标为待审核以便手动筛选”。目前我们暂时不用考虑优先级问题，保持默认的“10”即可。勾选“编译规则”和“启用规则”（后者勾选时会自动勾选前者）以确保规则生效，接下来我们开始设置规则内容。</p>
+                <p>这里仅讲解默认的“所见即所得编辑模式”的使用方法，“规则文本编辑模式”的用法将在后续“进阶指导”以及“高级技巧”部分展开。在这个模式下，一切都是语义化的，规则可以按照字面意思来理解。</p>
+                <p>为了书写规则内容，首先需要构思规则的逻辑。要检测评论中的网址，被检测的对象应该是“评论内容”。下表列出了目前所有可供检测的对象：</p>
+            </Box>
+            {/* TODO：这里应该有个表 */}
+            <Box fontSize="body1.fontSize" m={1}>
+                <p>考虑最简单的检测方法，可以使用“包含”运算符，检查评论内容是否包含“http”。如果是，标记为待审核；否则无动作。输入这些内容并保存，我们就完成了我们的第一条最简单的规则。</p>
+                <p>但是这样的缺陷十分明显：误判率太高。可能访客输入的是“HTTP 协议”，而“包含”既不区分大小写，也不关注上下文，尽管速度相对较快，但局限性比较大。即使更改为检测“http://”和“https://”，也同样存在很大问题。因此，我们可以考虑用正则表达式优化这条规则。</p>
+                <p>要使用正则表达式，首先需要把运算符改为“符合（正则表达式）”，然后书写正则表达式即可。简单的网址匹配可以使用 <code>/https?:\/\/[a-zA-Z0-9]+\.[a-zA-Z0-9]+/</code>，注意这个正则表达式并不是对网址的完整匹配，仅仅是一个特征识别。如果需要一个完整匹配，可以查阅正则表达式的相关资料。</p>
+                <p>有时，博主可能会在评论中发送网址，但这同样会被标记为待审核，显得很麻烦。我们可以通过增加判断逻辑来解决这个问题：先检测是否是博主，如果是则直接通过评论，否则再进行后续检测。这个修改比较棘手，因为它涉及判断逻辑的插入，需要重写所有的内容（简便方法将在后续介绍）。首先，将 <code>#Main</code> 中改为“如果 UID 等于 1”（一般情况下博主的 UID 为 1，游客为 0），“那么 通过评论 否则 继续判断”，在选择“继续判断”后，会立刻生成一个新的判断块，在该判断块中重写刚刚的“如果 评论内容 符合（正则表达式）……”即可。</p>
+                <p>保存规则，现在它已经具备简陋但很有效的功能了。这就是新增/编辑规则的一般步骤。</p>
+            </Box>
+            <Box fontSize="h5.fontSize" m={1}>
+                进阶指导
+                <Box color="text.secondary" fontSize="subtitle2.fontSize" component="span" m={1}>这才是常规操作</Box>
+            </Box>
+            <Box fontSize="body1.fontSize" m={1}>
+                <p>这部分可以介绍比较复杂的规则……</p>
+            </Box>
+            <Box fontSize="h5.fontSize" m={1}>
+                高级技巧
+                <Box color="text.secondary" fontSize="subtitle2.fontSize" component="span" m={1}>Let's Speed Up!</Box>
+            </Box>
+            <Box fontSize="body1.fontSize" m={1}>
+                <p>这里介绍 Rule 语法以及编译器相关……</p>
+            </Box>
+            <Box fontSize="h5.fontSize" m={1}>
+                疑难解答
+                <Box color="text.secondary" fontSize="subtitle2.fontSize" component="span" m={1}>好像有哪里不对</Box>
+            </Box>
+            <Box fontSize="body1.fontSize" m={1}>如果您在使用过程中遇到了问题，这部分内容可能会有所帮助。如果您无法在这里找到答案，可以<Link href="https://github.com/wuxianucw/Typecho-CommentRuleset/issues" target="_blank" rel="noopener noreferrer">在 GitHub 上提出 issue</Link>。</Box>
+            <Box fontSize="h6.fontSize" m={1}>
+                常见问题
+                <Box color="text.secondary" fontSize="subtitle2.fontSize" component="span" m={1}>Q&amp;A</Box>
+            </Box>
+            <Box fontSize="body1.fontSize" m={1}></Box>
+            <Box fontSize="h6.fontSize" m={1}>
+                编译错误参考
+                <Box color="text.secondary" fontSize="subtitle2.fontSize" component="span" m={1}>解析时遇到了雾之湖的妖精</Box>
+            </Box>
+            <Box fontSize="body1.fontSize" m={1}>注：下表中 <code>xxx</code> 代表任意字符。</Box>
         </Typography>
     );
 }
