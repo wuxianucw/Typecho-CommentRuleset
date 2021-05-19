68648b97fca43e2f25fad5cc8e2a2859038c324f
wuxianucw <x-wuxian@qq.com>
2021-05-19 17:10:12 +0800

    :pencil2: 更友好的日志展现

--- libs/Logger.php
+++ libs/Logger.php
@@ -3,7 +3,7 @@ namespace CommentRuleset;
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
  * Typecho 评论规则集插件 日志记录器
- * 
+ *
  * @package CommentRuleset
  * @author wuxianucw
  * @license GNU Affero General Public License v3.0
@@ -12,14 +12,14 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 class Logger {
     /**
      * 日志等级常量
-     * 
+     *
      * @var int
      */
     const VERBOSE = 0, DEBUG = 1, INFO = 2, WARNING = 3, ERROR = 4;
 
     /**
      * 日志等级
-     * 
+     *
      * @access protected
      * @var int
      */
@@ -27,7 +27,7 @@ class Logger {
 
     /**
      * 日志文件
-     * 
+     *
      * @access protected
      * @var resource
      */
@@ -35,7 +35,7 @@ class Logger {
 
     /**
      * 构造日志记录器对象
-     * 
+     *
      * @access public
      * @param int $level 日志等级 默认为 `Logger::INFO`
      * @param string $file 日志文件路径 如果为空则默认为 runtime 目录下以当前日期命名的 .log 文件
@@ -51,7 +51,7 @@ class Logger {
 
     /**
      * 销毁日志记录器对象
-     * 
+     *
      * @access public
      * @return void
      */
@@ -61,62 +61,62 @@ class Logger {
 
     /**
      * 记录一个 `verbose` 级别的日志
-     * 
+     *
      * @access public
      * @param string $text
      * @return void
      */
     public function verbose($text) {
         if ($this->level > self::VERBOSE) return;
-        fprintf($this->file, "[%s][V] %s\n", date('Y-m-d H:i:s'), $text);
+        fprintf($this->file, "[%s] [V] %s\n", date('Y-m-d H:i:s'), $text);
     }
 
     /**
      * 记录一个 `debug` 级别的日志
-     * 
+     *
      * @access public
      * @param string $text
      * @return void
      */
     public function debug($text) {
         if ($this->level > self::DEBUG) return;
-        fprintf($this->file, "[%s][D] %s\n", date('Y-m-d H:i:s'), $text);
+        fprintf($this->file, "[%s] [D] %s\n", date('Y-m-d H:i:s'), $text);
     }
 
     /**
      * 记录一个 `info` 级别的日志
-     * 
+     *
      * @access public
      * @param string $text
      * @return void
      */
     public function info($text) {
         if ($this->level > self::INFO) return;
-        fprintf($this->file, "[%s][I] %s\n", date('Y-m-d H:i:s'), $text);
+        fprintf($this->file, "[%s] [I] %s\n", date('Y-m-d H:i:s'), $text);
     }
 
     /**
      * 记录一个 `warning` 级别的日志
-     * 
+     *
      * @access public
      * @param string $text
      * @return void
      */
     public function warning($text) {
         if ($this->level > self::WARNING) return;
-        fprintf($this->file, "[%s][W] %s\n", date('Y-m-d H:i:s'), $text);
+        fprintf($this->file, "[%s] [W] %s\n", date('Y-m-d H:i:s'), $text);
     }
 
     /**
      * 记录一个 `error` 级别的日志
-     * 
+     *
      * @access public
      * @param string $text
      * @return void
      */
     public function error($text) {
         if ($this->level > self::ERROR) return;
-        fprintf($this->file, "[%s][E] %s\n", date('Y-m-d H:i:s'), $text);
+        fprintf($this->file, "[%s] [E] %s\n", date('Y-m-d H:i:s'), $text);
     }
 }
 
@@ -135,7 +135,7 @@ class NoLog extends Logger {
 
 /**
  * Logger 工厂函数
- * 
+ *
  * @return CommentRuleset\Logger
  */
 function logger() {
