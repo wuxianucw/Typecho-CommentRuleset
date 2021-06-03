3dc577145614decd4121d4cc1c7145c59dba7548
wuxianucw <x-wuxian@qq.com>
2021-06-03 10:49:20 +0800

    :tada: v0.1.1

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  *
  * @package CommentRuleset
  * @author wuxianucw
- * @version 0.1.0
+ * @version 0.1.1
  * @license GNU Affero General Public License v3.0
  * @link https://ucw.moe/
  */
@@ -15,7 +15,7 @@ class CommentRuleset_Plugin implements Typecho_Plugin_Interface {
      *
      * @var string
      */
-    const VERSION = '0.1.0';
+    const VERSION = '0.1.1';
 
     /**
      * 评论通过标志
